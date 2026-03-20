87560048d97f1ac871082d357e917d9176d27a20
silviomorais1 <silviomorais142@gmail.com>
2026-03-20 21:12:21 +0000

    fix: use bcryptjs for Railway compatibility

--- server.js
+++ server.js
@@ -8,7 +8,7 @@
 require('dotenv').config();
 const express    = require('express');
 const mysql      = require('mysql2/promise');
-const bcrypt     = require('bcrypt');
+const bcrypt     = require('bcryptjs');
 const jwt        = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
 const helmet     = require('helmet');
